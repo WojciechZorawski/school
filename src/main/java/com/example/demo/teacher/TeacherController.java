83a500241f8316553419f446e6441bbfa634c8bf
Wojciech Żórawski <wojciech.zorawski99@gmail.com>
2022-02-15 16:35:02 +0100

    Add CRUD for Teacher

--- src/main/java/com/example/demo/teacher/TeacherController.java
+++ src/main/java/com/example/demo/teacher/TeacherController.java
@@ -1,5 +1,52 @@
 package com.example.demo.teacher;
 
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/teachers")
+@RestController
+@RequiredArgsConstructor
 public class TeacherController {
 
+  private final TeacherService teacherService;
+
+  @GetMapping("/{id}")
+  public TeacherDTO getTeacherById(@PathVariable UUID id) {
+    TeacherDTO teacher = teacherService.getTeacherById(id);
+    return teacher;
+
+  }
+
+  @GetMapping("/list")
+  public List<TeacherDTO> getAllTeacher() {
+    List<TeacherDTO> list = teacherService.getAllTeachers();
+    return list;
+  }
+
+  @PostMapping
+  public TeacherDTO createTeacher(@RequestBody TeacherDTO newTeacher) {
+    TeacherDTO createdTeacher = teacherService.createTeacher(newTeacher);
+    return createdTeacher;
+  }
+
+  @PutMapping("/{id}")
+  public TeacherDTO updateTeacher(@PathVariable UUID id, @RequestParam String lastName, @RequestParam int age) {
+    TeacherDTO updatedTeacher = teacherService.updateTeacher(id, lastName, age);
+    return updatedTeacher;
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteTeacher(@PathVariable UUID id) {
+    teacherService.deleteTeacher(id);
+  }
 }
--- src/main/java/com/example/demo/teacher/TeacherService.java
+++ src/main/java/com/example/demo/teacher/TeacherService.java
@@ -1,28 +1,57 @@
 package com.example.demo.teacher;
 
+import com.example.demo.exception.BusinessLogicException;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TeacherService {
 
-//  public Teacher createTeacher(Teacher newTeacher) {
-//    Teacher createdTeacher =
-//    teachers.add(newTeacher);
-//    return teachers;
-//
-//  }
-//
-//  public Teacher updateTeacher(UUID id, String lastName, int age) throws ClassNotFoundException {
-//    Teacher updatedTeacher = findById(id);
-//    updatedTeacher.setLastName(lastName);
-//    updatedTeacher.setAge(age);
-//    return updatedTeacher;
-//
-//  }
-//  public String deleteTeacher(UUID id) throws ClassNotFoundException{
-//    Teacher deletedTeacher = findById(id);
-//    teachers.remove(deletedTeacher);
-//    return "Teacher with " + id + "was deleted";
-//  }
+  TeacherRepositoryFaker teacherRepository = new TeacherRepositoryFaker();
+
+  public TeacherDTO getTeacherById(UUID id) {
+    Teacher takenTeacher;
+    try {
+      takenTeacher = teacherRepository.findById(id);
+    } catch (ClassNotFoundException e) {
+      throw new BusinessLogicException("Bledne id");
+    }
+    return takenTeacher.toDto();
+  }
+
+  public List<TeacherDTO> getAllTeachers() {
+    List<TeacherDTO> takenTeachers = teacherRepository.findAll().stream()
+        .map(teacher -> teacher.toDto())
+        .collect(Collectors.toList());
+    return takenTeachers;
+  }
+
+  public TeacherDTO createTeacher(TeacherDTO newTeacher) {
+    try {
+      Teacher save = teacherRepository.save(newTeacher.toEntity());
+      return save.toDto();
+    } catch (ClassNotFoundException e) {
+      throw new BusinessLogicException("Nie udalo sie utworzyc nowego nauczyciela");
+    }
+  }
+
+  public TeacherDTO updateTeacher(UUID id, String lastName, int age) {
+    Teacher updatedTeacher = null;
+    try {
+      updatedTeacher = teacherRepository.findById(id);
+    } catch (ClassNotFoundException e) {
+      throw new BusinessLogicException("Bledne id");
+    }
+    updatedTeacher.setLastName(lastName);
+    updatedTeacher.setAge(age);
+    return updatedTeacher.toDto();
+
+  }
+
+  public void deleteTeacher(UUID id){
+    teacherRepository.deleteById(id);
+  }
 
 }
