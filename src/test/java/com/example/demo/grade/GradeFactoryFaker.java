0cecd07f93dfd74757dbd8c14ad556b0f86261fe
Wojciech Żórawski <wojciech.zorawski99@gmail.com>
2022-03-01 13:31:35 +0100

    Create test for grade package, add validation to GradeDTO.

---
+++ src/test/java/com/example/demo/grade/GradeFactoryFaker.java
@@ -0,0 +1,39 @@
+package com.example.demo.grade;
+
+import com.github.javafaker.Faker;
+import java.time.ZoneId;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+public class GradeFactoryFaker {
+
+  private static final Faker FAKER = Faker.instance(new Random(81));
+
+  static GradeDTO getValidGradeDto() {
+    return GradeDTO.builder()
+                   .date(FAKER.date().past(2021, TimeUnit.DAYS).toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+                   .grade(Integer.parseInt(FAKER.regexify("([1-6]){1}")))
+                   .subject(FAKER.name().name())
+                   .build();
+  }
+
+  static Grade getValidGradeEntity() {
+    return Grade.builder()
+                .id(UUID.randomUUID())
+                .date(FAKER.date().past(2021, TimeUnit.DAYS).toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+                .grade(Integer.parseInt(FAKER.regexify("([1-6]){1}")))
+                .subject(FAKER.name().name())
+                .build();
+  }
+
+  static GradeDTO getInvalidGradeDto() {
+    return GradeDTO.builder()
+                   .date(FAKER.date().future(2021, TimeUnit.DAYS).toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+                   .grade(Integer.parseInt(FAKER.numerify("##")))
+                   .subject(FAKER.name().name())
+                   .build();
+  }
+
+}
+
